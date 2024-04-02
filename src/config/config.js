73af661c69022ae11f9b96f9ef6dfcc0fe9230f5
amine <95883429+aminechairi@users.noreply.github.com>
2024-04-02 15:59:35 +0000

    this commit only for a test

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
-const baseUrl = 'http://localhost:8000/api/v1'; 
+// const baseUrl = 'http://localhost:8000/api/v1';
+const baseUrl = 'https://node-js-ecommerse-api-v1.vercel.app/api/v1';
 
 export default baseUrl;
