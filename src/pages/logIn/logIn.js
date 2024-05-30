3fc7acc0e5c4f85caff439b03334e0426d6104f9
amine <95883429+aminechairi@users.noreply.github.com>
2024-05-30 15:23:35 +0100

    Log in form validation with yup library.

--- src/pages/logIn/logIn.js
+++ src/pages/logIn/logIn.js
@@ -1,9 +1,37 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import "./logIn.css";
 
 import NavBar from "../../components/navBar/navBar";
 import Footer from "../../components/footer/footer";
 
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required.")
+    .email("Please provide a valid email address."),
+  password: Yup.string().required("Password is required."),
+});
+
 function LogIn() {
+  
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm({
+        values: {
+          email: "",
+          password: "",
+        },
+      });
+    },
+  });
+
   return (
     <>
       <NavBar />
@@ -11,7 +39,7 @@ function LogIn() {
         <div className="container">
           <div className="ab">
             <h1 className="title">log in</h1>
-            <form className="form">
+            <form className="form" onSubmit={formik.handleSubmit}>
               {/* Email input */}
               <div className="ab_inputs">
                 <label className="label" htmlFor="email">
@@ -21,11 +49,19 @@ function LogIn() {
                   className="input"
                   type="email"
                   placeholder="Email"
+                  name="email"
                   id="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  style={{
+                    borderColor: 
+                    formik.touched.email && formik.errors.email ? "red" : null,
+                  }}
                 />
-                <p className="error">
-                  Please, first name must be 16 charachter
-                </p>
+                {formik.touched.email && formik.errors.email ? (
+                  <p className="error">{formik.errors.email}</p>
+                ) : null}
               </div>
 
               {/* Password input */}
@@ -37,11 +73,19 @@ function LogIn() {
                   className="input"
                   type="password"
                   placeholder="Password"
+                  name="password"
                   id="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  style={{
+                    borderColor: 
+                    formik.touched.email && formik.errors.email ? "red" : null,
+                  }}
                 />
-                <p className="error">
-                  Please, first name must be 16 charachter
-                </p>
+                {formik.touched.password && formik.errors.password ? (
+                  <p className="error">{formik.errors.password}</p>
+                ) : null}
               </div>
 
               <input className="submit" type="submit" defaultValue="Submit" />
