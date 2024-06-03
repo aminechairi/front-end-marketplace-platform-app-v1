1d551adb30c6a334ca00c2faeb48a9a63d5bba83
amine <95883429+aminechairi@users.noreply.github.com>
2024-06-03 18:39:46 +0100

    edite config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const mode = "dev";
 let baseUrl;
 
-if (mode === "dev") {
+if (mode === "prod") {
   baseUrl = "http://localhost:8000/api/v1";
 } else {
   baseUrl = "https://node-js-ecommerse-api-v1.vercel.app/api/v1";
