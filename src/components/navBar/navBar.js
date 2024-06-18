241ff1665b22d4b8842300e462895d96a56c3ed5
amine <95883429+aminechairi@users.noreply.github.com>
2024-06-18 19:14:00 +0100

    Nav bar menu.

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,9 +1,12 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LoginIcon from "@mui/icons-material/Login";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 import "./navBar.css";
 import ScrollToTop from "../common/scrollToTop/scrollToTop";
@@ -12,6 +15,33 @@ import cookieManager from "../../utils/cookieManager";
 const auth = cookieManager("get", "JWTToken");
 
 export default function NavBar() {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [menuStyle, setMenuStyle] = useState({
+    opacity: 0,
+    visibility: "hidden",
+    transform: "translateY(-10px)",
+  });
+
+  useEffect(() => {
+    if (menuVisible) {
+      setMenuStyle({
+        opacity: 1,
+        visibility: "visible",
+        transform: "translateY(0)",
+      });
+    } else {
+      setMenuStyle({
+        opacity: 0,
+        visibility: "hidden",
+        transform: "translateY(-10px)",
+      });
+    }
+  }, [menuVisible]);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <div className="navBar">
       <div className="container">
@@ -54,8 +84,33 @@ export default function NavBar() {
                   <AccountCircleIcon />
                 </button>
               </div>
-              <div className="icon">
+              <div className="icon" onClick={toggleMenu}>
                 <MenuIcon />
+                <div className="ab_menu" style={menuStyle}>
+                  <ul className="menu">
+                    <li>
+                      <ScrollToTop>
+                        <Link to="/sign-up">
+                          <AccountCircleIcon /> profile
+                        </Link>
+                      </ScrollToTop>
+                    </li>
+                    <li>
+                      <ScrollToTop>
+                        <Link to="/log-in">
+                          <FavoriteBorderIcon /> saves
+                        </Link>
+                      </ScrollToTop>
+                    </li>
+                    <li>
+                      <ScrollToTop>
+                        <Link to="/sign-up">
+                          <ShoppingCartCheckoutIcon /> shopping cart
+                        </Link>
+                      </ScrollToTop>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           ) : (
@@ -72,8 +127,26 @@ export default function NavBar() {
                   </Link>
                 </ScrollToTop>
               </div>
-              <div className="icon">
+              <div className="icon" onClick={toggleMenu}>
                 <MenuIcon />
+                <div className="ab_menu" style={menuStyle}>
+                  <ul className="menu">
+                    <li>
+                      <ScrollToTop>
+                        <Link to="/log-in">
+                          <LoginIcon /> log in
+                        </Link>
+                      </ScrollToTop>
+                    </li>
+                    <li>
+                      <ScrollToTop>
+                        <Link to="/sign-up">
+                          <ExitToAppIcon /> sign up
+                        </Link>
+                      </ScrollToTop>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           )}
